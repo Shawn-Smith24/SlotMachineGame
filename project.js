4c6ffc7cdd1892f3f47a9e139ebdc2f83bafef02
shawn <shawnsmith0024@gmail.com>
2023-07-05 22:29:15 -0400

    stater code and plan

---
+++ project.js
@@ -0,0 +1,14 @@
+// 1. Deposit some money 
+// 2. Determine amount of lines user wants to bet on 
+// 3. Collect a bet amount from user
+// 4. Spin the slot machine 
+// 5. Check if the user won 
+// 6. Give the user their winnings or take if they lose 
+// 7. Play again
+
+
+const deposit = () => {
+    return 1
+}
+
+deposit();
