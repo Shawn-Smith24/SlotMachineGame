284dd4dcb7f137183559084631aac3d02db2095f
shawn <shawnsmith0024@gmail.com>
2023-07-06 13:56:52 -0400

    bet setup

--- project.js
+++ project.js
@@ -6,9 +6,62 @@
 // 6. Give the user their winnings or take if they lose 
 // 7. Play again
 
+const prompt = require("prompt-sync")();
+
+
+
+
+
+
+
+
 
 const deposit = () => {
-    return 1
+    //loops until the user provides a number 
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid amount, try again")
+        }
+        else {
+            return numberDepositAmount;
+        }
+    }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to be on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again")
+        }
+        else {
+            return numberOfLines;
+        }
+    }
 }
 
-deposit();
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the bet per lines: ");
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again")
+        }
+        else {
+            return numberBet;
+        }
+
+    }
+};
+
+
+    let balance = deposit();
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
